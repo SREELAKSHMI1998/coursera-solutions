cb692fafc59193cb912848bae1b2355865172ba3
SREELAKSHMI T J <47878337+SREELAKSHMI1998@users.noreply.github.com>
2020-05-04 23:11:31 +0530

    Update script.js

--- module_4/script.js
+++ module_4/script.js
@@ -1,5 +1,6 @@
 (function (window) {
-var names = ["athul", "John", "kiran", "sree", "hari", "jaise", "sofia", "ankit", "ediee"];
+var names =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 for (var i in names) {
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
 
